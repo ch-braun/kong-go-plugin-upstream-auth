2bf9464c4834db3e91da134c31ddb06c53347fa7
Christopher Braun <christopher.braun@telekom.de>
2024-11-14 09:23:40 +0100

    Added Basic Authentication Method. (#3)
    * Added Basic Authentication Method.
    
    * Added error logging.

---
+++ go-upstream-auth/basic.go
@@ -0,0 +1,28 @@
+package go_upstream_auth
+
+import (
+	"encoding/base64"
+	"github.com/Kong/go-pdk"
+	"github.com/Kong/go-pdk/log"
+)
+
+func AddBasicAuth(kong *pdk.PDK, username string, password string) {
+	_ = kong.Log.Debug("go-upstream-auth: AddBasicAuth")
+	defer func(Log log.Log, args ...interface{}) { _ = Log.Debug(args) }(kong.Log, "go-upstream-auth: AddBasicAuth complete")
+
+	// Check if the username and password are empty
+	if username == "" || password == "" {
+		_ = kong.Log.Warn("go-upstream-auth: Username or password is empty")
+		return
+	}
+
+	// Encode the username and password in base64
+	base64Encoded := base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
+
+	// Add the basic auth header
+	err := kong.ServiceRequest.SetHeader("Authorization", "Basic "+base64Encoded)
+	if err != nil {
+		_ = kong.Log.Err("go-upstream-auth: Error setting header: ", err)
+		return
+	}
+}
